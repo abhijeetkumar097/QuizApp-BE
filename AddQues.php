c1b6f30f9aaf968653f819a37226b69cecc10150
abhijeetkumar097 <aabhijeet097@gmail.com>
2024-11-21 00:35:22 +0530

    specialSymbol handling added

--- AddQues.php
+++ AddQues.php
@@ -9,27 +9,51 @@ require 'connectdp.php';
 $response = ['success' => false];
 $data = json_decode(file_get_contents('php://input'), true);
 
-if (isset($data['question'])) {
-    $question = $data['question'];
-    $a = $data['option_a'];
-    $b = $data['option_b'];
-    $c = $data['option_c'];
-    $d = $data['option_d'];
-    $answer = $data['answer'];
-    $subject = $data['subject'];
-    $username = $data['username'];
+// if (isset($data['question'])) {
+//     $question = $data['question'];
+//     $a = $data['option_a'];
+//     $b = $data['option_b'];
+//     $c = $data['option_c'];
+//     $d = $data['option_d'];
+//     $answer = $data['answer'];
+//     $subject = $data['subject'];
+//     $username = $data['username'];
 
-    $query = "INSERT INTO question (ques, option_a, option_b, option_c, option_d, answer, subject, username) VALUES ('$question', '$a', '$b', '$c', '$d', '$answer', '$subject', '$username')";
+//     $query = "INSERT INTO question (ques, option_a, option_b, option_c, option_d, answer, subject, username) VALUES ('$question', '$a', '$b', '$c', '$d', '$answer', '$subject', '$username')";
+
+//     if (mysqli_query($conn, $query)) {
+//         $response['success'] = true;
+//         $response['message'] = 'Question added successfully';
+//     } else {
+//         $response['message'] = 'Error adding question: '. mysqli_error($conn);
+//     }
+// } else {
+//     $response['message'] = 'Incomplete data provided';
+// }
+
+if (isset($data['question'])) {
+    $question = mysqli_real_escape_string($conn, $data['question']);
+    $a = mysqli_real_escape_string($conn, $data['option_a']);
+    $b = mysqli_real_escape_string($conn, $data['option_b']);
+    $c = mysqli_real_escape_string($conn, $data['option_c']);
+    $d = mysqli_real_escape_string($conn, $data['option_d']);
+    $answer = mysqli_real_escape_string($conn, $data['answer']);
+    $subject = mysqli_real_escape_string($conn, $data['subject']);
+    $username = mysqli_real_escape_string($conn, $data['username']);
+
+    $query = "INSERT INTO question (ques, option_a, option_b, option_c, option_d, answer, subject, username) 
+              VALUES ('$question', '$a', '$b', '$c', '$d', '$answer', '$subject', '$username')";
 
     if (mysqli_query($conn, $query)) {
         $response['success'] = true;
         $response['message'] = 'Question added successfully';
     } else {
-        $response['message'] = 'Error adding question: '. mysqli_error($conn);
+        $response['message'] = 'Error adding question: ' . mysqli_error($conn);
     }
 } else {
     $response['message'] = 'Incomplete data provided';
 }
+
 
 echo json_encode($response);
 
